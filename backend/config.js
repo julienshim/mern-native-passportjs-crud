d4403e434653a5b976c5fd3f31cb2421e16b3b4d
jerauld <25723428+jerauld@users.noreply.github.com>
2018-07-12 15:22:32 -0700

    adding initial files

---
+++ backend/config.js
@@ -0,0 +1,5 @@
+export const google = {
+    clientID: '250572172932-sv5dmlsvfsq4g5vu96l03sa5tlnvnrus.apps.googleusercontent.com',
+    clientSecret: 'hqOkE8_RvEuOwR8qTEHLzicw',
+    callbackURL: 'http://localhost:3000/auth/google/callback',
+};
