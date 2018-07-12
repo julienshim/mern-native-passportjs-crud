f7185e0b9ce65afbf220e3eea603001c56477cea
jerauld <25723428+jerauld@users.noreply.github.com>
2018-07-12 15:54:37 -0700

    deleted config file

--- backend/config.js
+++
@@ -1,5 +0,0 @@
-export const google = {
-    clientID: '250572172932-sv5dmlsvfsq4g5vu96l03sa5tlnvnrus.apps.googleusercontent.com',
-    clientSecret: 'hqOkE8_RvEuOwR8qTEHLzicw',
-    callbackURL: 'http://localhost:3000/auth/google/callback',
-};
