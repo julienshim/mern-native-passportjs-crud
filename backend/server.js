a24d28e6649f61818f7aa3a05f955e3d216aed54
jerauld <25723428+jerauld@users.noreply.github.com>
2018-07-13 16:10:36 -0700

    converted server to SSL/https

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,7 @@
 import express from 'express';
+import fs from 'fs';
+import https from 'https';
+import path from 'path';
 import passport from 'passport';
 import FacebookStrategy from 'passport-facebook';
 import GoogleStrategy from 'passport-google-oauth20';
@@ -40,6 +43,7 @@ passport.deserializeUser((user, done) => done(null, user));
 
 // Initialize http server
 const app = express();
+const PORT = 3000;
 
 // Initialize Passport
 app.use(passport.initialize());
@@ -61,7 +65,18 @@ app.get('/auth/google/callback',
   (req, res) => res.redirect('OAuthLogin://login?user=' + JSON.stringify(req.user)));
 
 // Launch the server on the port 3000
-const server = app.listen(3000, () => {
-  const { address, port } = server.address();
-  console.log(`Listening at http://${address}:${port}`);
-});
+// const server = app.listen(3000, () => {
+//   const { address, port } = server.address();
+//   console.log(`Listening at http://${address}:${port}`);
+// });
+
+const httpsOptions = {
+  cert: fs.readFileSync(path.join(__dirname, 'ssl', './server-crt.pem')),
+  key: fs.readFileSync(path.join(__dirname, 'ssl', './server-key.pem')),
+  ca: fs.readFileSync(path.join(__dirname, 'ssl', './ca-crt.pem')),
+}
+
+https.createServer(httpsOptions, app)
+  .listen(PORT, function() {
+    console.log(`App is listening at https://localhost:${PORT}`);
+  });
