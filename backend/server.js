26bfaa0887b1f17d0958670df216c01843291815
jerauld <25723428+jerauld@users.noreply.github.com>
2018-07-13 21:47:26 -0700

    moved configuration to separate folder

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ import passport from 'passport';
 import FacebookStrategy from 'passport-facebook';
 import GoogleStrategy from 'passport-google-oauth20';
 // Import Facebook and Google OAuth apps configs
-import { facebook, google } from './config';
+import { facebook, google } from './config/config';
 
 // Transform Facebook profile because Facebook and Google profile objects look different
 // and we want to transform them into user objects that have the same set of attributes
