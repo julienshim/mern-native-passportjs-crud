0fb4b3f374b8d298b19e7de67a3a5f50ff90946d
jerauld <25723428+jerauld@users.noreply.github.com>
2018-07-13 21:59:59 -0700

    moved passport configuration to config folder

--- backend/server.js
+++ backend/server.js
@@ -1,45 +1,9 @@
 import express from 'express';
+import passportSetup from './config/passport-setup';
 import fs from 'fs';
 import https from 'https';
 import path from 'path';
 import passport from 'passport';
-import FacebookStrategy from 'passport-facebook';
-import GoogleStrategy from 'passport-google-oauth20';
-// Import Facebook and Google OAuth apps configs
-import { facebook, google } from './config/config';
-
-// Transform Facebook profile because Facebook and Google profile objects look different
-// and we want to transform them into user objects that have the same set of attributes
-const transformFacebookProfile = (profile) => ({
-  name: profile.name,
-  avatar: profile.picture.data.url,
-});
-
-// Transform Google profile into user object
-const transformGoogleProfile = (profile) => ({
-  name: profile.displayName,
-  avatar: profile.image.url,
-});
-
-// Register Facebook Passport strategy
-passport.use(new FacebookStrategy(facebook,
-  // Gets called when user authorizes access to their profile
-  async (accessToken, refreshToken, profile, done)
-    // Return done callback and pass transformed user object
-    => done(null, transformFacebookProfile(profile._json))
-));
-
-// Register Google Passport strategy
-passport.use(new GoogleStrategy(google,
-  async (accessToken, refreshToken, profile, done)
-    => done(null, transformGoogleProfile(profile._json))
-));
-
-// Serialize user into the sessions
-passport.serializeUser((user, done) => done(null, user));
-
-// Deserialize user from the sessions
-passport.deserializeUser((user, done) => done(null, user));
 
 // Initialize http server
 const app = express();
