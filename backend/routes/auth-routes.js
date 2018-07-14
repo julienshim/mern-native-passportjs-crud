26848f70c41e7cb8055284c1159fa69ff374d4ee
jerauld <25723428+jerauld@users.noreply.github.com>
2018-07-14 01:33:25 -0700

    expanding passportjs to include email

--- backend/routes/auth-routes.js
+++ backend/routes/auth-routes.js
@@ -4,7 +4,7 @@ import passport from 'passport';
 const router = express.Router();
 
 // Set up Facebook auth routes
-router.get('/facebook', passport.authenticate('facebook'));
+router.get('/facebook', passport.authenticate('facebook', {scope: ['email']}));
 
 router.get('/facebook/callback',
   passport.authenticate('facebook', { failureRedirect: '/facebook' }),
@@ -12,7 +12,7 @@ router.get('/facebook/callback',
   (req, res) => res.redirect('OAuthLogin://login?user=' + JSON.stringify(req.user)));
 
 // Set up Google auth routes
-router.get('/google', passport.authenticate('google', { scope: ['profile'] }));
+router.get('/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
 
 router.get('/google/callback',
   passport.authenticate('google', { failureRedirect: '/google' }),
